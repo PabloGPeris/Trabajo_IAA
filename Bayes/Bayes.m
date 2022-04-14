f89a4b532d6090868e2a719664c3fa5ac7873b7e
Javier Díaz Mena <javidiaz94@gmail.com>
2022-04-14 20:03:16 +0200

    Revert "Merge branch 'main' of https://github.com/PabloGPeris/Trabajo_IAA"
    This reverts commit 1fabf744e6c5760b523ef4878edc5ae522bedd7b, reversing
    changes made to 181f69696c5506797dcb34765d238f2cf44cac50.

--- Bayes/Bayes.m
+++ Bayes/Bayes.m
@@ -1,13 +1,10 @@
 %% Trabajo Inteligencia Artificial Aplicada
-%% First Bayes
-% Aquí se entrena el clasificador bayesiano - esto de llamarle a todo
-% FirstAlgo quizás debería parar - llamarle trainBayes o algo así
+%% Múltiples Bayes para hacer pruebas
 clear
 
 addpath("..\")
 load Trainnumbers.mat % para la clasificación básicamente
-load("datos_PCA.mat", "data_pca", "latent"); % sin hacer PCA previa
-% load("datos_PCA.mat", "data_pca_r"); % data_r_pca es lo interesante
+load("datos_PCA.mat", "data_pca"); % sin hacer PCA previa
 
 %% Datos
 % dimensiones de la PCA
@@ -16,40 +13,81 @@ PCA = 20;
 % tanto por uno de datos que se usan para entrenar (no para test)
 PD = 0.8;
 
-%% PCA previa (nº de dimensiones)
-% coge solo las dimensiones requeridas en la PCA
-data_r_pca = data_pca(:, 1:PCA)';
+% número de iteraciones en el bucle
+I = 10;
 
-% MSE esperado
-MSE_esperado = (sum(latent) - cumsum(latent))/sum(latent);
-MSE = MSE_esperado(PCA)
-
-%% Separar datos en train y test
+%% PCA
 % nº datos
 N = length(Trainnumbers.label); 
 
-% los datos se mezclan (permutan y se separan)
-ind_random = randperm(N);
+accuracy = 0;
+conf_mat = zeros(10, 10);
 
-% train
-data_train = data_r_pca(:, ind_random(1:round(N*PD)));
-label_train = Trainnumbers.label(ind_random(1:round(N*PD)));
+for i = 1:I
+    %% PCA previa (nº de dimensiones)
+    % coge solo las dimensiones requeridas en la PCA
+    data_r_pca = data_pca(:, 1:PCA)';
 
-% test
-data_test = data_r_pca(:, ind_random(round(N*PD)+1:end));
-label_test = Trainnumbers.label(ind_random(round(N*PD)+1:end));
+    %% Separar datos en train y test aleatoriamente
+    % los datos se mezclan (permutan y se separan)
+    ind_random = randperm(N);
+    
+    % train
+    data_train = data_r_pca(:, ind_random(1:round(N*PD)));
+    label_train = Trainnumbers.label(ind_random(1:round(N*PD)));
+    
+    % test
+    data_test = data_r_pca(:, ind_random(round(N*PD)+1:end));
+    label_test = Trainnumbers.label(ind_random(round(N*PD)+1:end));
 
-%% Clasificador bayesiano
-% train
-bayesModel = fitcnb(data_train', label_train', 'Prior', ones(1, 10));
+    %% Clasificador bayesiano
+    % train
+    bayesModel = fitcnb(data_train', label_train', 'Prior', ones(1, 10));
+%     bayesModel = fitcnb(data_train', label_train', 'Prior', ones(1, 10), ...
+%         'OptimizeHyperparameters', 'auto');
+%     bayesModel = fitcnb(data_train', label_train', 'Prior', ones(1, 10), ...
+%         'DistributionNames', 'kernel', 'Kernel', 'triangle' );
+%     bayesModel = fitcnb(data_train', label_train', 'Prior', ones(1, 10), ...
+%         'DistributionNames', {'mvnm','mvmn'});
 
-% test
-label_pred = predict(bayesModel, data_test')';
+    % test (classification)
+    label_pred = predict(bayesModel, data_test')';
 
-% matriz de confusión
+    accuracy = accuracy + sum(label_test == label_pred)/round(N*(1-PD));
+    conf_mat = conf_mat + confusionmat(label_test, label_pred);
+
+    disp("iteration " + num2str(i) + "/" + num2str(I))
+end
+
+accuracy = accuracy / I
+
+%% Figuras
 figure(11);
-conf_mat = confusionchart(label_test, label_pred);
+confusionchart(conf_mat, 0:9, ...
+    'ColumnSummary','column-normalized', ...
+    'RowSummary','row-normalized');
 
-accuracy = trace(conf_mat.NormalizedValues)/round(N*(1-PD))
+figure(12);
+confusionchart(conf_mat, 0:9, ...
+    'ColumnSummary','absolute', ...
+    'RowSummary','absolute');
 
-save Bayes.m bayesModel PCA
+%% Clasificación - Errores
+% n = 8;
+% d_pca = [15 16];
+% 
+% ind_TP_U_FP = label_pred == n; % predicted positive
+% ind_TP_U_FN = label_test == n; % is positive
+% ind_TP = ind_TP_U_FP & ind_TP_U_FN; % true positive
+% ind_FP = ind_TP_U_FP & ~ind_TP; % false positive
+% ind_FN = ind_TP_U_FN & ~ind_TP; % false negative
+% 
+% figure(13);
+% plot(data_train(d_pca(1),ind_TP), data_train(d_pca(2),ind_TP), 'x', 'LineWidth', 1.5)
+% hold on
+% plot(data_train(d_pca(1),ind_FP), data_train(d_pca(2),ind_FP), 'x', 'LineWidth', 1.5)
+% plot(data_train(d_pca(1),ind_FN), data_train(d_pca(2),ind_FN), 'x', 'LineWidth', 1.5)
+% hold off
+% xlabel('PCA 1')
+% ylabel('PCA 2')
+% legend('TP', 'FP', 'FN')
