8e14642a7e2106afab6adb789906436136a86108
Luis236855 <62139556+Luis236855@users.noreply.github.com>
2022-04-15 17:04:09 +0200

    Segunda version LDA
    Algo falla, en la evolución del MSE a menor numero de dimensiones parece que mejor, no se muestran las imagenes de los datos reconstruidos.

---
+++ FirstPCA.m
@@ -0,0 +1,93 @@
+%% Trabajo Inteligencia Artificial Aplicada
+%% First PCA
+% Primera PCA que se hace, donde se obtienen los coeficientes y todas esas
+% cosas de la PCA, guardando tanto los datos como los coeficientes y el
+% número de dimensiones. Este script sirve, sobre todo, para las
+% representaciones gráficas;
+clear
+%load("datos_normalizacion.mat", "data_n");
+load datos_normalizacion.mat
+%% Variables
+
+% en tanto por uno, máximo error de reconstrucción que queremos poner
+MSE = 0.7;
+% otra opción: decir el número de variables que quieres directamente
+% PCA = 50;
+% comentar el que no quieras
+
+% esta variable indica si se quiere hacer la reconstrucción o no
+rec = true; 
+
+%% PCA
+% hace el PCA
+% coeff_pca = autovectores
+% data_pca = datos en las dimensiones de la PCA (transpuesto)
+% latent = autovalores = MSE esperado
+[coeff_pca, data_pca, latent] = pca(data_n'); 
+
+% calcula el MSE esperado al quedarnos concada dimensión (en pu) - La suma
+% de latent es, evidente, el número de dimensiones total = 673, pero queda
+% más claro así puesto
+MSE_esperado = (sum(latent) - cumsum(latent))/sum(latent);
+
+if ~exist('D_pca', 'var')
+    % número de dimensiones de PCA con las que nos quedamos según el MSE
+    % admisible
+    PCA = find(MSE_esperado <= MSE, 1)
+    
+    % datos ya de dimensionalidad reducida
+    data_r_pca = data_pca(:, 1:PCA)'; 
+    
+    % otra forma de obtener los datos de dimensionalidad reducida sería:
+    % data_r_pca = coeff_pca(:,1:D_pca)'*data_n ;
+    
+    figure(1);
+    plot(0:length(latent), [1 MSE_esperado'], 'LineWidth', 1.5);
+    xline(D_pca, 'HandleVisibility','off')
+    yline(MSE_admisible, 'HandleVisibility','off')
+    xlabel('nº de dimensiones')
+    ylabel('MSE esperado (por unidad)')
+    xlim([0 length(latent)])
+    ylim([0 inf])
+else
+    % datos ya de dimensionalidad reducida
+    %data_r_pca = data_pca(:, 1:PCA)'; 
+
+    % MSE esperado que cometemos
+    MSE = MSE_esperado(PCA)
+
+%     figure(1);
+%     plot(0:length(latent), [1 MSE_esperado'], 'LineWidth', 1.5);
+%     xline(D_pca, 'HandleVisibility','off')
+%     yline(MSE_admisible, 'HandleVisibility','off')
+%     xlabel('nº de dimensiones')
+%     ylabel('MSE esperado (por unidad)')
+%     xlim([0 length(latent)])
+%     ylim([0 inf])
+end
+
+save datos_PCA data_pca coeff_pca PCA data_r_pca latent
+
+%% Reconstrucción
+% vale para ver si el número de dimensiones es suficiente
+if rec  %#ok<*UNRCH>  
+    load Trainnumbers.mat
+    N = width(Trainnumbers.image); % nº datos
+    D_inicial = height(Trainnumbers.image); % nº dim iniciales
+
+    % datos reconstruidos normalizados
+    data_rec_n = coeff_pca(:, 1:PCA)*data_r_pca;
+
+    % datos reconstruidos desnormalizados
+    data_rec = zeros(D_inicial, N);
+    data_rec(ind_validos, : ) = data_rec_n.*sigma_validos + media_validos;
+    
+    rp = randperm(N, 4);
+    figure(2);
+    for i = 1:4
+        subplot(2, 4, i*2 - 1)
+        digit_display(Trainnumbers.image, rp(i))
+        subplot(2, 4, i*2)
+        digit_display(data_rec, rp(i))
+    end
+end
--- scatter_matrices.m
+++ scatter_matrices.m
@@ -6,11 +6,19 @@ SC = cell(1, nC);
 mC = cell(1, nC);
 SB = 0;
 SW = 0;
+msum = 0;
+
 for i = 1:nC
     mC{i} = mean(sep_data{i}, 2);
+    %msum = msum + mC{i};    
     SC{i} = (sep_data{i}-mC{i})*(sep_data{i}-mC{i})';
     SW = SW + SC{i};
     SB = SB + length(sep_data{i})*mC{i}*mC{i}';
 end
 
+%m = msum/nC;
+% SB1 = 0;
+% for i = 1:nC      %Da exactamente igual a SB
+%     SB1 = SB1 + length(sep_data{i})*(mC{i}-m)*(mC{i}-m)';
+% end
 end
