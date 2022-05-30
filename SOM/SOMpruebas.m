3269f0a04a006f7a0507f9566c274732855abe9f
Javier Díaz Mena <javidiaz94@gmail.com>
2022-05-30 11:57:52 +0200

    SOM test y SOMpruebas puestos bonitos
    y SOM pruebas recuperado que me lo habia cargado sin querer

--- SOM/SOMpruebas.m
+++ SOM/SOMpruebas.m
@@ -7,12 +7,12 @@ close all
 addpath("..\")
 load Trainnumbers.mat
 load("datos_PCA.mat", "data_pca"); % sin hacer PCA previa
-load("datos_PCA_test.mat", "data_pca_test");
 
 etiquetas = Trainnumbers.label;
 PCA = 25;
+PD = 0.8; % porcentaje (tanto por uno) de datos de training (80/20 tipico)
 
-% Creamos mapa SOM bidimensional
+% Crear un mapa SOM bidimensional
 anchura = 30;
 altura = 36;
 n_neuronas = anchura*altura;
@@ -23,13 +23,26 @@ net.trainParam.showWindow = 1; % 0 = Cierra ventana de visualizacion
 
 % PCA previa solo nº dimensiones requeridas en la PCA
 data_r_pca = data_pca(:, 1:PCA)';
-data_r_pca_test = data_pca_test(:, 1:PCA)';
+
+%% Separacion 80/20 del data_r_pca
+
+N = length(Trainnumbers.label); % nº datos
+ind_random = randperm(N); % los datos se mezclan (permutan y se separan)
+
+% datos de entrenamiento
+data_train = data_r_pca(:, ind_random(1:round(N*PD)));
+label_train = etiquetas(ind_random(1:round(N*PD)));
+
+% datos de test
+data_test = data_r_pca(:, ind_random(round(N*PD)+1:end));
+label_test = etiquetas(ind_random(round(N*PD)+1:end));
+
 
 %% Entrenar SOM
-net = train(net,data_r_pca);
+net = train(net,data_train);
 
 %% Asignar valor (una etiqueta) a cada neurona
-p_sep = class_separation(data_r_pca, etiquetas); % función mía
+p_sep = class_separation(data_train, label_train); % función mía
 
 n_clases = length(p_sep);
 n_apariciones = zeros(n_clases, n_neuronas);
@@ -47,16 +60,18 @@ end
 
 %% Evaluacion de la red
 
-evaluacion = net(data_r_pca_test);
+evaluacion = net(data_test);
 neuronas_activadas_test = vec2ind(evaluacion);
 
 % Mira a qué clase pertenece esa neurona, y lo asigna
 clase_predicha = clase(neuronas_activadas_test) - 1;
 
-%% Guardado
+%% Matriz de confusión
 
-class = clase_predicha;
-name = {'LuisBF', 'PabloGP', 'JavierDM'};
-% PCA = 25;
-save('Group1_som.mat', "name", "PCA", "class")
+conf_mat = confusion(label_test, clase_predicha);
+figure(3)
+conf_chart = confusionchart(label_test, clase_predicha);
 
+% Calcular accuracy
+accuracy = trace(conf_chart.NormalizedValues)/(N*(1-PD));
+disp(accuracy*100)
